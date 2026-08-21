d647aa8ab92bd0f50d8ac75b6b5f377aabd8dea4
aa-r-ju <aarju3447@gmail.com>
2026-08-21 15:29:37 +0545

    feat: object-properties

--- tests/1-testfirst-part-1/02-object-properties/object-properties.js
+++ tests/1-testfirst-part-1/02-object-properties/object-properties.js
@@ -1,1 +1,37 @@
 /* eslint-disable no-unused-vars */
+function setPropsOnObj(obj) {
+  obj.x = 7;
+  obj.y = 8;
+  obj.onePlus = function (num) {
+    return num + 1;
+  };
+}
+
+function setPropsOnArr(arr) {
+  arr.hello = function () {
+    return "Hello!";
+  };
+
+  arr.full = "stack";
+  arr[0] = 5;
+  arr.twoTimes = function (para) {
+    return para * 2;
+  };
+}
+
+function setPropsOnFunc(fun) {
+  fun.year = "20??";
+  fun.divideByTwo = function (num) {
+    return num / 2;
+  };
+}
+
+function shallowCopy(para1, para2) {
+  if (Array.isArray(para1) && Array.isArray(para2)) {
+    return [...para1, ...para2];
+  }
+
+  if (typeof para1 === "object" && typeof para2 === "object") {
+    return { ...para1, ...para2 };
+  }
+}
