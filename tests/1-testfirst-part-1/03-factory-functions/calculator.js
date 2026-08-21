22ac612087a32c2b9fd814dba69c6ab97b999740
aa-r-ju <aarju3447@gmail.com>
2026-08-21 16:22:24 +0545

    feat: create-Calculator

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,1 +1,24 @@
 /* eslint-disable no-unused-vars */
+function createCalculator() {
+  let total = 0;
+  return {
+    value: function () {
+      return total;
+    },
+    add: function (value) {
+      return (total += value);
+    },
+    subtract: function (value) {
+      return (total -= value);
+    },
+    clear: function () {
+      total = 0;
+    },
+  };
+}
+
+let result = createCalculator();
+console.log(result);
+console.log(result.add(5));
+console.log(result.add(5));
+console.log(result.subtract(3));
