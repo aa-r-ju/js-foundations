f631ee3320836d63a043cdad7bda3f58e47b6e8e
aa-r-ju <aarju3447@gmail.com>
2026-08-30 22:02:06 +0545

    feat: create-book

--- tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
+++ tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
@@ -1,1 +1,45 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+
+const bookPrototype = {
+  getPrice: function () {
+    return this.price;
+  },
+
+  getInfo: function () {
+    return `${this.title} by ${this.author}`;
+  },
+
+  addRating: function (rate) {
+    this.rating.push(rate);
+  },
+
+  getRating: function () {
+    let sum = 0;
+
+    for (let i = 0; i < this.rating.length; i++) {
+      sum += this.rating[i].length;
+    }
+
+    return sum / this.rating.length;
+  },
+};
+
+function createBook(id, title, author, price) {
+  const book = Object.create(bookPrototype);
+
+  book.id = id;
+  book.title = title;
+  book.author = author;
+  book.price = price;
+  book.rating = [];
+
+  return book;
+}
+
+const book = createBook(1, "Catch 22", "Joseph Heller", 19.99);
+console.log(book.hasOwnProperty("id"));
+console.log(book.hasOwnProperty("title"));
+console.log(book.hasOwnProperty("author"));
+console.log(book.hasOwnProperty("price"));
+console.log(book.hasOwnProperty("rating"));
+console.log(book.hasOwnProperty("getInfo"));
