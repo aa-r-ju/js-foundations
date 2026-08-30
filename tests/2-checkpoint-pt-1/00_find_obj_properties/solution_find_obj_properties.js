fad20dfa619f232224349c28a130db3c1724b63d
aa-r-ju <aarju3447@gmail.com>
2026-08-30 15:13:23 +0545

    feat: findObjPropsHasOwn

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function findObjPropsHasOwn(obj) {
+  let result = "";
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (result !== "") {
+        result += ", ";
+      }
+      result += key;
+    }
+  }
+  return result;
+}
