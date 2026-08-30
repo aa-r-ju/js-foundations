d86d6d6675494a0f3c93f11440d0348bad924d93
aa-r-ju <aarju3447@gmail.com>
2026-08-30 21:30:41 +0545

    feat: multiplicationTable

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function multiplicationTable(row, column) {
+  let arr = [];
+
+  for (let i = 1; i <= row; i++) {
+    let temp = [];
+    for (let j = 1; j <= column; j++) {
+      temp.push(j * i);
+    }
+    arr.push(temp);
+  }
+  return arr;
+}
