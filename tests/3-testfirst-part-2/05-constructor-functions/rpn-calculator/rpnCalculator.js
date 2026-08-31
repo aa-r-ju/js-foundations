8c05de2909096b778b15c7699944a1572eda6245
aa-r-ju <aarju3447@gmail.com>
2026-08-31 23:25:43 +0545

    feat: rpnCalculator

--- tests/3-testfirst-part-2/05-constructor-functions/rpn-calculator/rpnCalculator.js
+++ tests/3-testfirst-part-2/05-constructor-functions/rpn-calculator/rpnCalculator.js
@@ -1,1 +1,57 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+
+function RPNCalculator() {
+  this.stack = [];
+}
+
+RPNCalculator.prototype.push = function (number) {
+  this.stack.push(number);
+};
+
+RPNCalculator.prototype.plus = function () {
+  if (this.stack.length < 2) {
+    throw "rpnCalculatorInstance is empty";
+  }
+
+  const right = this.stack.pop();
+  const left = this.stack.pop();
+
+  this.stack.push(left + right);
+};
+
+RPNCalculator.prototype.minus = function () {
+  if (this.stack.length < 2) {
+    throw "rpnCalculatorInstance is empty";
+  }
+
+  const right = this.stack.pop();
+  const left = this.stack.pop();
+
+  this.stack.push(left - right);
+};
+
+RPNCalculator.prototype.times = function () {
+  if (this.stack.length < 2) {
+    throw "rpnCalculatorInstance is empty";
+  }
+
+  const right = this.stack.pop();
+  const left = this.stack.pop();
+
+  this.stack.push(left * right);
+};
+
+RPNCalculator.prototype.divide = function () {
+  if (this.stack.length < 2) {
+    throw "rpnCalculatorInstance is empty";
+  }
+
+  const right = this.stack.pop();
+  const left = this.stack.pop();
+
+  this.stack.push(left / right);
+};
+
+RPNCalculator.prototype.value = function () {
+  return this.stack[this.stack.length - 1];
+};
