1c918aca1dfdd37e5e620239c256a12d72643514
aa-r-ju <aarju3447@gmail.com>
2026-09-01 22:06:20 +0545

    feat: simulateBattle

--- tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
+++ tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -14,3 +14,27 @@ Pokemon.prototype.isDefeated = function () {
   }
   return true;
 };
+function simulateBattle(pokemon1, pokemon2, firstAttacker) {
+  let attacker;
+  let defender;
+
+  if (pokemon1.name === firstAttacker) {
+    attacker = pokemon1;
+    defender = pokemon2;
+  } else {
+    attacker = pokemon2;
+    defender = pokemon1;
+  }
+
+  while (true) {
+    defender.health = defender.health - attacker.biteAttack();
+
+    if (defender.isDefeated()) {
+      return attacker.name + " Wins!";
+    }
+
+    const temp = attacker;
+    attacker = defender;
+    defender = temp;
+  }
+}
