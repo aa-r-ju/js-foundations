7ec992149503b1fa65b31594b03bc103a0f6d47f
aa-r-ju <aarju3447@gmail.com>
2026-09-03 16:04:31 +0545

    feat: forEach

--- tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
+++ tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+function forEach(arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    fn(arr[i]);
+  }
+}
+
+forEach(["soccer", "swimming", "softball", "water-polo"], (sport) =>
+  console.log(sport)
+);
