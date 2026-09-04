0eac527926f7f86af4f8611b0eff47952c107c6f
aa-r-ju <aarju3447@gmail.com>
2026-09-04 17:46:13 +0545

    feat: capitalize & swapCase

--- tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -1,1 +1,25 @@
 /* eslint-disable no-unused-vars */
+function indexAndValue(arr) {
+  return arr.map((value, index) => {
+    return { index: index, value: value };
+  });
+}
+function capitalize(str) {
+  return str.toUpperCase();
+}
+console.log(capitalize("hello world"));
+
+function swapCase(sentence) {
+  let arr = sentence.split(" ");
+  let result = arr.map((val, index) => {
+    if (index % 2 === 0) {
+      return capitalize(val);
+    } else {
+      return val;
+    }
+  });
+
+  return result.join(" ");
+}
+
+console.log(swapCase("i am going to the movies today"));
