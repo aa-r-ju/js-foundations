9e647800e547e590c5c1758d20e24fb7492a023d
aa-r-ju <aarju3447@gmail.com>
2026-09-07 17:29:54 +0545

    feat: runNumTimes

--- tests/4-checkpoint-pt-2/00_runNumTimes/solution_runNumTimes.js
+++ tests/4-checkpoint-pt-2/00_runNumTimes/solution_runNumTimes.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+function runNumTimes(fn, runTimes) {
+  for (let i = 0; i < runTimes; i++) {
+    fn();
+  }
+}
+
+let myString = "";
+
+runNumTimes(() => {
+  myString += "hello";
+  return myString;
+}, 3);
+
+console.log(myString);
