c22818b418ee3202b1d4e33d495f4addc7fdfb45
aa-r-ju <aarju3447@gmail.com>
2026-09-09 13:34:20 +0545

    feat: RPNCalculator

--- tests/5-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/5-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,61 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+
+class RPNCalculator {
+  constructor() {
+    this.arr = [];
+  }
+
+  push(val) {
+    this.arr.push(val);
+  }
+
+  value() {
+    return this.arr[this.arr.length - 1];
+  }
+
+  plus() {
+    if (this.arr.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    }
+
+    let second = this.arr.pop();
+    let first = this.arr.pop();
+
+    this.arr.push(first + second);
+  }
+
+  minus() {
+    if (this.arr.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    }
+
+    let second = this.arr.pop();
+    let first = this.arr.pop();
+
+    this.arr.push(first - second);
+  }
+
+  times() {
+    if (this.arr.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    }
+
+    let second = this.arr.pop();
+    let first = this.arr.pop();
+
+    this.arr.push(first * second);
+  }
+
+  divide() {
+    if (this.arr.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    }
+
+    let second = this.arr.pop();
+    let first = this.arr.pop();
+
+    this.arr.push(first / second);
+  }
+}
