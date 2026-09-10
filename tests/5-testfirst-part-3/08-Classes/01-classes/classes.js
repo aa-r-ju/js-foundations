b1c58aa475d684d607866f50494428e42f6c9b65
aa-r-ju <aarju3447@gmail.com>
2026-09-10 22:01:26 +0545

    feat: Cat

--- tests/5-testfirst-part-3/08-Classes/01-classes/classes.js
+++ tests/5-testfirst-part-3/08-Classes/01-classes/classes.js
@@ -6,11 +6,30 @@ class Mammal {
   }
 
   sayHello() {
-    return `My name is ${this.name}, I'm a Mammal`;
+    return `My name is ${this.name}, I'm a ${this.constructor.name}`;
   }
 
   haveBaby() {
     let child = new Mammal(`Baby ${this.name}`);
+
+    this.offspring.push(child);
+
+    return child;
+  }
+}
+
+class Cat extends Mammal {
+  constructor(name, color) {
+    super(name);
+    this.color = color;
+  }
+
+  meow() {
+    return "meow";
+  }
+
+  haveBaby(color) {
+    let child = new Cat(`Baby ${this.name}`, color);
 
     this.offspring.push(child);
 
