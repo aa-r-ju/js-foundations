9af8dadce8946fa0f982ed10a974174099e3a60a
aa-r-ju <aarju3447@gmail.com>
2026-09-12 14:07:35 +0545

    feat: Laptop

--- tests/5-testfirst-part-3/08-Classes/02_macbooks/macbooks.js
+++ tests/5-testfirst-part-3/08-Classes/02_macbooks/macbooks.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+class Laptop {
+  constructor(year, size) {
+    this.year = year;
+    this.hd = size;
+  }
+
+  checkSpecs() {
+    return `Year: ${this.year}, HD: ${this.hd}`;
+  }
+}
+
+let kk = new Laptop(1005, 500);
+console.log(kk.checkSpecs());
