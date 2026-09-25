92ca8c1ce2e3ab2a20777e312f7e1d508cc64032
aa-r-ju <aarju3447@gmail.com>
2026-09-25 20:52:34 +0545

    feat: Vehicle

--- tests/7-testfirst-review/classes/00-vehicles/vehicle.js
+++ tests/7-testfirst-review/classes/00-vehicles/vehicle.js
@@ -1,1 +1,32 @@
 /* eslint-disable no-unused-vars */
+// class Vehicle {
+//   constructor(arr) {
+//     this.wheelTotal = arr[0];
+//     this.energySource = arr[1];
+//     this.manufacturer = arr[2];
+//     this.isOn = false;
+//   }
+
+//   getSpec(key) {
+//     return this[key];
+//   }
+// }
+
+class Vehicle {
+  constructor(arr) {
+    this.wheelTotal = arr[0];
+    this.energySource = arr[1];
+    this.manufacturer = arr[2];
+    this.isOn = false;
+  }
+
+  getSpec(key) {
+    if (this.hasOwnProperty(key)) {
+      return this[key];
+    }
+
+    return undefined;
+  }
+}
+let kk = new Vehicle([2, "gas", "Harly-Davidson"]);
+console.log(kk.getSpec("energySource"));
