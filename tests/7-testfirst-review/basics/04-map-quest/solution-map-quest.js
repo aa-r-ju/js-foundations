608df330a16a4b287b4998fe780db21c60c98315
aa-r-ju <aarju3447@gmail.com>
2026-09-25 13:33:03 +0545

    feat: mapQuest

--- tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
+++ tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+function mapQuest(arr) {
+  return arr.reduce((result, current) => {
+    let last = result[result.length - 1];
+    if (
+      (last === "E" && current === "W") ||
+      (last === "W" && current === "E") ||
+      (last === "S" && current === "N") ||
+      (last === "N" && current === "S")
+    ) {
+      result.pop();
+    } else {
+      result.push(current);
+    }
+    return result;
+  }, []);
+}
+
+console.log(mapQuest(["S", "E", "W", "W"]));
