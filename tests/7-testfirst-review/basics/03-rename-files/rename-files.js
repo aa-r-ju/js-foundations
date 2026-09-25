5a01818c86943e3b1a6f6a4d24a214ba33295c98
aa-r-ju <aarju3447@gmail.com>
2026-09-25 13:04:41 +0545

    feat:renameFiles

--- tests/7-testfirst-review/basics/03-rename-files/rename-files.js
+++ tests/7-testfirst-review/basics/03-rename-files/rename-files.js
@@ -1,1 +1,40 @@
 /* eslint-disable no-unused-vars */
+function renameFiles(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i]);
+    } else {
+      for (let count = 1; count < arr.length; count++) {
+        let newName = arr[i] + "(" + count + ")";
+
+        if (!newArr.includes(newName)) {
+          newArr.push(newName);
+          break;
+        }
+      }
+    }
+  }
+
+  return newArr;
+}
+console.log(renameFiles(["file", "fileTwo", "fileThree", "fileFour"]));
+console.log(renameFiles(["FullstackTestFirst", "GuessingGame", "FileWatcher"]));
+console.log(renameFiles(["hello", "world", "hello"]));
+console.log(
+  renameFiles([
+    "a(1)",
+    "a(6)",
+    "a",
+    "a",
+    "a",
+    "a",
+    "a",
+    "a",
+    "a",
+    "a",
+    "a",
+    "a",
+  ])
+);
