e4d61e71504ba73cfb4a2ba6e47950f61d4d400f
aa-r-ju <aarju3447@gmail.com>
2026-09-25 21:49:37 +0545

    feat: Tesla

--- tests/7-testfirst-review/classes/00-vehicles/vehicle.js
+++ tests/7-testfirst-review/classes/00-vehicles/vehicle.js
@@ -1,17 +1,4 @@
 /* eslint-disable no-unused-vars */
-// class Vehicle {
-//   constructor(arr) {
-//     this.wheelTotal = arr[0];
-//     this.energySource = arr[1];
-//     this.manufacturer = arr[2];
-//     this.isOn = false;
-//   }
-
-//   getSpec(key) {
-//     return this[key];
-//   }
-// }
-
 class Vehicle {
   constructor(arr) {
     this.wheelTotal = arr[0];
@@ -28,5 +15,25 @@ class Vehicle {
     return undefined;
   }
 }
-let kk = new Vehicle([2, "gas", "Harly-Davidson"]);
-console.log(kk.getSpec("energySource"));
+
+class Tesla extends Vehicle {
+  constructor(arr) {
+    super(arr[4][0]);
+
+    this.milesPerCharge = arr[0];
+    this.model = arr[1];
+    this.chargePercentage = arr[2];
+    this.minutesToCharge = arr[3];
+  }
+
+  minutesToFullCharge(currentCharge) {
+    let remainingPercentage = 100 - currentCharge;
+    let minutes =
+      (this.minutesToCharge / (100 - this.chargePercentage)) *
+      remainingPercentage;
+
+    return `${minutes} minutes until the charge is full!`;
+  }
+}
+let teslaCar = new Tesla([300, "Model S", 50, 100, [[4, "electric", "Tesla"]]]);
+console.log(teslaCar);
