f0b8631e0b87a204d0f15b274849ec5d04889730
aa-r-ju <aarju3447@gmail.com>
2026-09-26 14:52:13 +0545

    feat: accessor

--- tests/7-testfirst-review/closure/00-accessor/accessor.js
+++ tests/7-testfirst-review/closure/00-accessor/accessor.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+function accessor(obj) {
+  return function (value, resetValue) {
+    if (resetValue === undefined) {
+      return obj[value];
+    }
+    obj[value] = resetValue;
+    return resetValue;
+  };
+}
