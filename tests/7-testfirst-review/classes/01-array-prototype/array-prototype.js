98d7bd58af55e5ede83b484b5da5929c94118df4
aa-r-ju <aarju3447@gmail.com>
2026-09-26 11:34:22 +0545

    feat: Array.prototype.fiilter

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,20 @@
 /* eslint-disable no-unused-vars */
+Array.prototype.maap = function (callback) {
+  let result = [];
+
+  this.forEach(function (val) {
+    result.push(callback(val));
+  });
+  return result;
+};
+
+Array.prototype.fiilter = function (callbacks) {
+  let filteredValue = [];
+
+  this.forEach(function (val) {
+    if (callbacks(val)) {
+      filteredValue.push(val);
+    }
+  });
+  return filteredValue;
+};
